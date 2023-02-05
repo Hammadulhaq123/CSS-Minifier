b82e3387013b85a5167d539106b38f9d9091c957
Hammadulhaq123 <professorcoding123@gmail.com>
2023-02-05 12:44:41 +0500

    Updated Buttons Styling

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -40,11 +40,11 @@ export default function TextForm(props) {
                 <textarea class="form-control" value={text} id="floatingTextarea2" onChange={change} row="8" style={{width:"80vw", alignSelf:"center", background:"#0000" ,color:"#fff", height: "30vh"}}></textarea>
                 <h3 className='mt-3' style={{color:"#fff"}}>{props.title2}</h3>
                 <textarea class="form-control" value={changedText} id="floatingTextarea2" row="8" style={{width:"80vw",height: "30vh", border:"2px solid #046ec4" ,alignSelf:"center", background:"#0000", color:"#046ec4"}}></textarea>
-                <div className='buttons d-flex flex-row flex-wrap mt-4'>
-                <button className='btn btn-primary mt-1 rounded mx-3' onClick={uppercase}>Text Uppercase</button>
-                <button className='btn btn-success mt-1 rounded mx-3' onClick={lowercase}>Text LowerCase</button>
-                <button className='btn btn-warning mt-1 rounded mx-3' onClick={minify}>Minify the CSS</button>
-                <button className='btn btn-danger mt-1 rounded mx-3' onClick={clear}>Clear Textfield</button>
+                <div className='buttons d-flex flex-row flex-wrap mt-4 justify-content-center align-items-center'>
+                <button className='btn btn-primary mt-1 col-2.5  rounded-30 mx-3' onClick={uppercase}>Text Uppercase</button>
+                <button className='btn btn-success mt-1  col-2.5 rounded-30 mx-3' onClick={lowercase}>Text LowerCase</button>
+                <button className='btn btn-warning mt-1  col-2.5 rounded-30 mx-3' onClick={minify}>Minify the CSS</button>
+                <button className='btn btn-danger mt-1 col-2.5  rounded-30 mx-3' onClick={clear}>Clear Textfield</button>
                 </div>
             </div>
         </>
